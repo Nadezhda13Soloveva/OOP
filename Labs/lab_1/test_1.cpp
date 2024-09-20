0fb3a17ade281d75b576b4e18d20fd99715303e1
Nadezhda <nadezhda13soloveva@mail.ru>
2024-09-20 19:50:00 +0300

    Correct tests

--- Labs/lab_1/test_1.cpp
+++ Labs/lab_1/test_1.cpp
@@ -3,27 +3,42 @@
 
 TEST(test_01, basic_test_set)
 {
-    ASSERT_TRUE(exchanger("abbaba")=="baabab");
+    std::string input = "abbaba";
+    std::string output = "baabab";
+    std::string result = exchanger(input);
+    ASSERT_TRUE(result == output);
 }
 
 TEST(test_02, basic_test_set)
 {
-    ASSERT_TRUE(exchanger("bbcabaca")=="aacbabcb");
+    std::string input = "bbcabaca";
+    std::string output = "aacbabcb";
+    std::string result = exchanger(input);
+    ASSERT_TRUE(result == output);
 }
 
 TEST(test_03, basic_test_set)
 {
-    ASSERT_TRUE(exchanger("aabcdab")=="\nОшибка: строка содержит недопустимый символ. Используйте только a, b и c.");
+    std::string input = "aabcdab";
+    std::string output = "\nОшибка: строка содержит недопустимый символ. Используйте только a, b и c.";
+    std::string result = exchanger(input);
+    ASSERT_TRUE(result == output);
 }
 
 TEST(test_04, basic_test_set)
 {
-    ASSERT_TRUE(exchanger("")=="\nОшибка: Введена пустая строка.");
+    std::string input = "";
+    std::string output = "\nОшибка: Введена пустая строка.";
+    std::string result = exchanger(input);
+    ASSERT_TRUE(result == output);
 }
 
 TEST(test_05, basic_test_set)
 {
-    ASSERT_TRUE(exchanger("aBBcACbAA")=="bAAcBCaBB");
+    std::string input = "aBBcACbAA";
+    std::string output = "bAAcBCaBB";
+    std::string result = exchanger(input);
+    ASSERT_TRUE(result == output);
 }
 
 int main(int argc, char **argv) {
