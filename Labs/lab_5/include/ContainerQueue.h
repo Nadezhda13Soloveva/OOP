34c1d54a9a8ee261ccd846aede22408eecf022b5
Nadezhda Soloveva <67784033+Nadezhda13Soloveva@users.noreply.github.com>
2024-11-15 16:34:05 +0300

    Update ContainerQueue.h

--- Labs/lab_5/include/ContainerQueue.h
+++ Labs/lab_5/include/ContainerQueue.h
@@ -63,7 +63,6 @@ public:
     public:
         using iterator_category = std::forward_iterator_tag;
         using value_type = T;
-        using difference_type = std::ptrdiff_t;
         using pointer = T*;
         using reference = T&;
 
